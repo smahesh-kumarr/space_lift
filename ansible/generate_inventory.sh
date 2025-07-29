2cc2ed75e241a4e53ce61acbfbd44b3d4982dcd6
smahesh-kumarr <maheshkumar08042006@gmail.com>
2025-07-29 14:26:37 +0530

    Made generate_inventory.sh executable

--- ansible/generate_inventory.sh
+++ ansible/generate_inventory.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PUBLIC_IPS=$(echo $TF_PUBLIC_IPS | jq -r '.[]' | paste -sd "," -)
-PRIVATE_IPS=$(echo $TF_PRIVATE_IPS | jq -r '.[]' | paste -sd "," -)
-BASTION_IP=$(echo $TF_PUBLIC_IPS | jq -r '.[0]') # Use first public instance as bastion
-cat ansible/inventory.tmpl | sed "s/\${public_ips}/$PUBLIC_IPS/" | sed "s/\${private_ips}/$PRIVATE_IPS/" | sed "s/\${bastion_ip}/$BASTION_IP/" > ansible/inventory.yml
+     PUBLIC_IPS=$(echo $TF_PUBLIC_IPS | jq -r '.[]' | paste -sd "," -)
+     PRIVATE_IPS=$(echo $TF_PRIVATE_IPS | jq -r '.[]' | paste -sd "," -)
+     BASTION_IP=$(echo $TF_PUBLIC_IPS | jq -r '.[0]') # Use first public instance as bastion
+     cat ansible/inventory.tmpl | sed "s/\${public_ips}/$PUBLIC_IPS/" | sed "s/\${private_ips}/$PRIVATE_IPS/" | sed "s/\${bastion_ip}/$BASTION_IP/" > ansible/inventory.yml
